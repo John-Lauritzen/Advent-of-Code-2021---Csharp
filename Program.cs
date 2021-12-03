2ab9c314b0a0d9a950c0679f2e1ca67105e06eb8
John-Lauritzen <92867440+John-Lauritzen@users.noreply.github.com>
2021-12-02 19:06:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Advent_of_Code_2021___Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter day to run:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    Day1();
+                    break;
+            }
+        }
+        static void Day1()
+        {
+
+        }
+    }
+    
+}
